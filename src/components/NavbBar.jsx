1e88f0cc7ae2826918639443da7e01b12250c0be
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-08-01 00:47:24 +0200

    .

---
+++ src/components/NavbBar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "../style/navbar.css"
+import { Button } from "react-bootstrap"
+
+export default function(){        
+    return (
+        <div className="container">
+            <div className="inner">
+                <h1>MATCHED BETTING BLACK</h1>
+                <p>Il metodo rivoluzionario per il Matched Betting per generare fino al 50% di ROI!</p>
+                <Button className="iscrivitiOra">Iscriviti Ora</Button>
+            </div>               
+        </div>
+    );
+}
