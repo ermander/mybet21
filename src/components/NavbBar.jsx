f2876268f3983ac5e9b34028639f8a88948914a7
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-08-01 20:25:47 +0200

    .

--- src/components/NavbBar.jsx
+++ src/components/NavbBar.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import "../style/navbar.css"
-import "../style/arrow.css"
 import { Button } from "react-bootstrap"
 
 export default function(){        
@@ -10,12 +9,6 @@ export default function(){
                 <h1>MATCHED BETTING BLACK</h1>
                 <p>Il metodo rivoluzionario per ottenere il 70% di bonus su ogni ricarica!</p>
                 <Button className="iscrivitiOra">Iscriviti Ora</Button>
-                <div className="scrolldown">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
             </div>               
         </div>
     );
