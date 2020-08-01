077726922732e7a9c4758994c286e39d516dc621
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-08-01 21:06:54 +0200

    .

--- src/components/BonusInfo.jsx
+++ src/components/BonusInfo.jsx
@@ -7,30 +7,47 @@ export default function() {
         <>
         <div className="container-fluid-bonusInfo">
             <Row className="container">
-                <Col xs={6}>
+                <Col xs={12}>
                     <Row className="faCirleRow">
-                        <Col>                        
+                        <Col xs={6}>                        
                             <div className="image"> 
                                 <span className="span">70%</span>
-                            </ div>
+                            </div>
+                        </Col>
+                        <Col xs={6} className="faCircleInfo">
+                            <p>Il valore del bonus per deposito!</p>
                         </Col>
                     </Row>
                     <Row className="faCirleRow">
-                        <Col>                       
+                        <Col xs={6}>                       
                             <div className="image"> 
                                 <span className="span">30€</span>
-                            </ div>
-                        </Col>                        
+                            </div>
+                        </Col>   
+                        <Col xs={6} className="faCircleInfo">
+                            <p>Il taglio massimo per ogni ricarica!</p>
+                        </Col>                     
                     </Row>
                     <Row className="faCirleRow">
-                        <Col>                    
+                        <Col xs={6}>                    
                             <div className="image"> 
                                 <span className="span">3</span>
-                            </ div>
-                        </Col>                        
+                            </div>
+                        </Col>    
+                        <Col xs={6} className="faCircleInfo">
+                            <p>I bookmakers convenzionati!</p>
+                        </Col>                    
                     </Row>
-                </Col>
-                <Col xs={6}>
+                    <Row className="faCirleRow">
+                        <Col xs={6}>                    
+                            <div className="image"> 
+                                <span className="span">Ꝏ</span>
+                            </div>
+                        </Col>    
+                        <Col xs={6} className="faCircleInfo">
+                            <p>INFINITO, il numero di ricare che puoi fare!</p>
+                        </Col>                    
+                    </Row>
                 </Col>
             </Row>
         </div>
