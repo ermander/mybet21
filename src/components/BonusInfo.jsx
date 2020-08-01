f9c62ec113a73a43939f47be9386e0978de2dc80
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-08-01 19:42:00 +0200

    .

--- src/components/BonusInfo.jsx
+++ src/components/BonusInfo.jsx
@@ -1,28 +1,32 @@
 import React from "react"
 import { Row, Col } from "react-bootstrap"
 import "../style/bonusInfo.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from "@fortawesome/free-solid-svg-icons"
 
 export default function() {
     return (
         <>
         <div className="container-fluid-bonusInfo">
-            <Row className="mainRow">
+            <Row className="container">
                 <Col xs={6}>
                     <Row className="faCirleRow">
                         <Col>                        
-                            <FontAwesomeIcon className="faCircle" icon={faCircle}/>
+                            <div className="image"> 
+                                <span className="span">70%</span>
+                            </ div>
                         </Col>
                     </Row>
                     <Row className="faCirleRow">
                         <Col>                       
-                         <FontAwesomeIcon className="faCircle" icon={faCircle}/>
+                            <div className="image"> 
+                                <span className="span">30€</span>
+                            </ div>
                         </Col>                        
                     </Row>
                     <Row className="faCirleRow">
                         <Col>                    
-                            <FontAwesomeIcon className="faCircle" icon={faCircle}/>
+                            <div className="image"> 
+                                <span className="span">3</span>
+                            </ div>
                         </Col>                        
                     </Row>
                 </Col>
